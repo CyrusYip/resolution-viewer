226123c5a5fb416fcb3183931759540864c85f9c
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2024-08-15 01:09:17 +0800

    feat: get window info

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
 
+function getWindowInfo() {
+  const { innerWidth, innerHeight, outerWidth, outerHeight, devicePixelRatio } = window
+  const { width, height, availWidth, availHeight } = window.screen
+  return { innerWidth, innerHeight, outerWidth, outerHeight, devicePixelRatio, width, height, availWidth, availHeight }
+}
+
+const windowInfo = ref(getWindowInfo())
+
+// Refresh info on resize event
+onMounted(() => {
+  window.addEventListener('resize', () => {
+    windowInfo.value = getWindowInfo()
+  })
+})
 </script>
 
 <template>
@@ -8,14 +23,25 @@
   </header>
 
   <main>
-    <p>main</p>
+    <p class="info" v-for="(value, key, index) in windowInfo" :key="index">
+      {{ key }}: {{ value }}
+    </p>
   </main>
 
   <footer>
-    <p>footer</p>
+    <!-- Create vertical scrollbar -->
+    <p>footer start</p>
+    <div class="scrollbar-y"></div>
+    <p>footer end</p>
   </footer>
 </template>
 
 <style scoped>
+.info {
+  color: #af3a03;
+}
 
+.scrollbar-y {
+  height: 1500px;
+}
 </style>
